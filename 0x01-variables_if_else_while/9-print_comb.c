2a965fb4f9b5840dd0b643ce7a3807f8583e712c
Smog-Olufolahan <smoglibrary@gmail.com>
2021-06-18 15:16:40 -0700

    Patience, persistence and perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,28 +1,26 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
- * main - Print all possible combinations of single digit numbers
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int number;
+	int i;
 
-	for (number = 0; number <= 9; number++)
+	for (i = 48; i < 58; i++)
 	{
-	putchar(number + '0');
-	if (number  <  9)
-	{
-	putchar(',');
-	putchar(' ');
-	}
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
 }
-
 
 
 
