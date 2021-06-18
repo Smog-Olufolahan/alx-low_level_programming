a9b9c06f73782720f44c212e7915d5c7f124c9a1
Smog-Olufolahan <smoglibrary@gmail.com>
2021-06-18 14:21:51 -0700

    Do what you do better...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,13 +8,14 @@
 int main(void)
 {
 	int i, j, k;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			for (k = 0; k < 10; k++)
 			{
-				if (i !=j && i < j && j < k)
+				if (i != j && i < j && j < k)
 				{
 					putchar (i + 48);
 					putchar (j + 48);
