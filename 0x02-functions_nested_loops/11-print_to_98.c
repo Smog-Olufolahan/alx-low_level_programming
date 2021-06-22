1d774ff05eefb1edfcc88d4f58a920fd399e361b
Smog-Olufolahan <smoglibrary@gmail.com>
2021-06-22 13:24:22 -0700

    Holberton School, Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - prints all natural numbers
@@ -13,19 +14,20 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (; n > 98; n--)
+		for (n = n; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
 	}
 	else if (n < 98)
 	{
-		for (; n < 98; n++)
+		for (n = n; n < 98; n++)
 		{
 			printf("%d, ", n);
 		}
 	}
 	printf("%d\n", n);
 }
+
 
 
