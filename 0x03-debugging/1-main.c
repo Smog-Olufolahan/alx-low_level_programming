69fffe085b218505c8583e68ffc577d93b28097c
Smog-Olufolahan <smoglibrary@gmail.com>
2021-06-23 14:44:15 -0700

    Infinite loopsss

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,7 +10,9 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
+
 	/**
 	 * while (i < 10)
 	 * {
@@ -18,6 +20,7 @@ int main(void)
 	 * }
 	 */
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
 
