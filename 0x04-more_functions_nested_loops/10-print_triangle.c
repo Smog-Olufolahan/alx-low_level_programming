f96e8aacae7ea7c9300f3c870090b45623a105f7
Smog-Olufolahan <smoglibrary@gmail.com>
2021-06-24 13:55:01 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,22 +2,36 @@
 
 /**
  * print_triangle - prints a triangle
- * @size: takes in a integer for size of triangle
+ * @size: print size
+ * Return: Always 0.
  */
 
 void print_triangle(int size)
 {
-	int row, column, k;
+	int i, z, d, p;
 
-	if (size <= 0)
-		_putchar('\n');
-	for (row = 0; row < size; row++)
+	if (size > 0)
 	{
-		for (column = size - row; column > 1; column--)
-			_putchar(' ');
-		for (k = row + column; k >= 1; K--)
-			_putchar('#');
-		_putchar('\n');
+		d = size - 1;
+		for (i = 0; i < size; i++)
+		{
+			for (z = d; z > 0; z--)
+			{
+				_putchar (' ');
+			}
+			for (p = 0; p <= i; p++)
+			{
+				_putchar (35);
+			}
+			d--;
+			_putchar ('\n');
+		}
+	}
+	else
+	{
+		_putchar ('\n');
 	}
 }
+
+
 
