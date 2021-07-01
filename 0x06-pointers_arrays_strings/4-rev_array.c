23d1c369b801d61407440604700c292383f5e40a
Smog-Olufolahan <smoglibrary@gmail.com>
2021-07-01 07:25:28 -0700

    A kind of paranoid in reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,13 +10,22 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, temp;
+	int *start_c, *end_c, c;
+	int i;
 
-	for (i = o; i < n--; i++)
+	start_c = a;
+	end_c = a;
+	for (i = 0; i < n - 1; i++)
 	{
-		temp = a[i];
-		a[i] = a[n];
-		a[n] = temp;
+		end_c++;
+	}
+	for (i = 0; i < n / 2; i++)
+	{
+		c = *end_c;
+		*end_c = *start_c;
+		*start_c = c;
+		start_c++;
+		end_c--;
 	}
 }
 
