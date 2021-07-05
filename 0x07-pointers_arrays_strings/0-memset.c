fe63de661aa453d0dd8bac78dc26895e1c71bc06
Smog-Olufolahan <smoglibrary@gmail.com>
2021-07-05 11:43:23 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/***
+ * _memset - fill a block of memory with a constant byte
+ * @s: starting address of memory to be filled
+ * @b: value to set
+ * @n: number of bytes to be changed
+ *
+ * Return: dest
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+# OxO7 . C - Even more pointers, arrays and strings
+## O. memset
+* A function that fills memory with a constant byte.
+## 
